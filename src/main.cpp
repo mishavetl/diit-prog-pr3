970047976ea782db61413c25f47a6b4e7cac9c59
Michael Vietluzhskykh <mishavetl@gmail.com>
2019-10-13 18:41:29 +0300

    update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,10 @@
 
 int main() {
     int n;
+    std::cout << "Enter the number of members to calculate (n): ";
     std::cin >> n;
 
+    std::cout << "sequence = ";
     lr3::sequence::calculate(std::cout, n);
 
     return 0;
